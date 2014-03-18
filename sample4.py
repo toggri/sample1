efe1d8cfd4011957a157a2b03ffe01486251c460
toggri <toggri@gmail.com>
2014-03-18 11:58:31 +0900

    add some sample phthon files

---
+++ sample4.py
@@ -0,0 +1,15 @@
+# There are some String method
+"""
+len(), lower(), upper(), str()
+str : straightforward It's turns non-strings into string!
+"""
+
+parrot = "Norwegian Blue"
+
+print len(parrot)
+
+print parrot.lower()
+
+print parrot.upper()
+
+print str(3.14)

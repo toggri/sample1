e0ca727a3fb56514fdc4735e596fadac58e4e30d
toggri <toggri@gmail.com>
2014-03-18 13:14:52 +0900

    String Formatting Part2

---
+++ sample5.py
@@ -0,0 +1,14 @@
+# Print the concatenation of "Spam and eggs" on line 3!
+
+print "Spam "+"and "+"eggs"
+
+# Turn 3.14 into a string on line 3!
+
+# print "The value of pi is around " + 3.14 // This is a error.
+print "The value of pi is around " + str(3.14)
+
+# String formatting with %
+string_1 = "Camelot"
+string_2 = "place"
+
+print "Let\'s not go to %s. \'Tis a silly %s." % (string_1, string_2)
